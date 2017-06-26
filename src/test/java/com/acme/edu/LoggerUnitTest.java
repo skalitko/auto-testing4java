020ca349178336de318e25dacd6d90e10c4e9ead
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-06-26 11:30:46 +0300

    Add fixture builder demo.

--- src/test/java/com/acme/edu/LoggerUnitTest.java
+++ src/test/java/com/acme/edu/LoggerUnitTest.java
@@ -7,39 +7,86 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 
 public class LoggerUnitTest {
     private LoggerFilter filterStub;
 
-    public void setUp(boolean filter) throws SQLException {
-        ResultSet rsStub = mock(ResultSet.class);
-        when(rsStub.next()).thenReturn(true);
-        when(rsStub.getString("column1")).thenReturn("value1");
-        when(rsStub.getString("column2")).thenReturn("value2");
-
-        Statement statementStub = mock(Statement.class);
-        when(statementStub.executeQuery("SELECT * FROM table")).thenReturn(rsStub);
-
-        Connection connectionStub = mock(Connection.class);
-        when(connectionStub.createStatement(0, 0))
-                .thenReturn(statementStub);
-
-        DataSource connectionFactoryStub = mock(DataSource.class);
-        when(connectionFactoryStub.getConnection()).thenReturn(connectionStub);
-    }
 
     @Test
     public void shouldDoLogWhenErrorMessage() {
-        LoggerSaver saverMock = mock(LoggerSaver.class);
+        DataSource connectionFactoryStub =
+                new ConnectionFactoryMockitoStubBuilder()
+                    .withConnection()
+                    .withStatement("SQL1")
+                        .withResultSet()
+                            .withRecord("column2:value1")
+                            .withRecord("column3:value2")
+                            .withRecord("column3:value3")
+                .build();
 
-        Logger sut = new Logger(filterStub, saverMock);
-        sut.log("ERROR");
+        new BusinessLogicComponent(connectionFactoryStub);
+    }
+}
 
-        verify(saverMock, times(2)).save("ERROR");
+class BusinessLogicComponent {
+    public BusinessLogicComponent(DataSource ds) {
+    }
+}
+
+class ConnectionFactoryMockitoStubBuilder {
+    private DataSource connectionFactoryStub;
+    private Connection connectionStub;
+    private Statement statementStub;
+    private String sql;
+    private ResultSet rsStub;
+    private List<String> records = new LinkedList<>();
+    private boolean hasRecords = true;
+
+    public ConnectionFactoryMockitoStubBuilder withConnection() {
+        connectionFactoryStub = mock(DataSource.class);
+        connectionStub = mock(Connection.class);
+        return this;
+    }
+
+    public ConnectionFactoryMockitoStubBuilder withStatement(String sql) {
+        this.sql = sql;
+        this.statementStub = mock(Statement.class);
+        return this;
+    }
+
+    public ConnectionFactoryMockitoStubBuilder withResultSet() {
+        rsStub = mock(ResultSet.class);
+        return this;
+    }
+
+    public ConnectionFactoryMockitoStubBuilder withRecord(String record) {
+        records.add(record);
+        return this;
+    }
+
+    public DataSource build() {
+        try {
+            when(rsStub.next()).thenReturn(hasRecords);
+            for (String record : records) {
+                String[] fields = record.split(":");
+                when(rsStub.getString(fields[0])).thenReturn(fields[1]);
+            }
+
+            when(statementStub.executeQuery(sql)).thenReturn(rsStub);
+
+            when(connectionStub.createStatement()).thenReturn(statementStub);
+
+            when(connectionFactoryStub.getConnection()).thenReturn(connectionStub);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connectionFactoryStub;
     }
 }
