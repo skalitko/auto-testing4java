8c2a891335fcdd20dde53b5e4d317ee2feed675f
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-09-11 12:03:09 +0300

    Add System Test Demo.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -9,6 +9,17 @@ public class Logger {
         this.loggerSaver = loggerSaver;
     }
 
+    /**
+     * Design by Contract:
+     * 1. Pre-conditions:
+     *  - message != null -> IllArgException
+     *  - filter != null && saver != null -> IllStateException
+     * 2. Post-conditions:
+     *  - Implementation-specific, private
+     *  - Public Side effect
+     *  - Interaction based on Mocks
+     * 3. Invariants
+     */
     public void log(String message) {
         if (loggerFilter.filter(message)) {
             loggerSaver.save(message);
---
+++ src/test/java/com/acme/edu/LoggerIT.java
@@ -0,0 +1,10 @@
+package com.acme.edu;
+
+import org.junit.Test;
+
+public class LoggerIT {
+    @Test //BDD = DDD + test
+    public void shouldLogSuccessfullyWhenStateandParamIsNotNull() { //Counter Anti-pattern
+
+    }
+}
