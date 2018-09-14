fed50fcca7b927e606ca6c2fea684151e856b373
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-09-14 11:53:15 +0300

    Add Unit Test with Assertions Demo.

--- src/test/java/com/acme/edu/ArrayListTest.java
+++ src/test/java/com/acme/edu/ArrayListTest.java
@@ -1,21 +1,36 @@
 package com.acme.edu;
 
-import org.junit.Ignore;
-import org.junit.Test;
+import org.hamcrest.CoreMatchers.*;
+import org.fest.assertions.api.Assertions;
+import org.junit.*;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.core.StringContains.*;
+import static junit.framework.TestCase.assertTrue;
+
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeTrue;
 
-import org.junit.Assume;
+import static org.fest.assertions.api.Assertions.assertThat;
 
 public class ArrayListTest {
+    private ArrayList<Object> sut;
+
+    @BeforeClass @AfterClass
+    public static void globalSetUp() {
+
+    }
+
+    @Before @After
+    public void setUp() {
+        sut = new ArrayList<>(); //Diamond operator
+    }
+
     @Test
     public void shouldSizeIncrementedWhenAddElementNotNull() {
         //region Given
-        ArrayList<Object> sut = new ArrayList<>();
         Object dummy = new Object();
         assumeTrue(sut.size() == 0);
         //endregion
@@ -27,6 +42,45 @@ public class ArrayListTest {
 
         //region Then
         assertEquals(1, sut.size());
+        //endregion
+    }
+
+    @Test//(expected = Throwable.class)
+    public void shouldThrowExceptionWhenAddNullElement() {
+        //region When
+        sut.add(null);
+        //endregion
+    }
+
+    @Test
+    public void shouldGetStringRepresentationFromElementsWhenToString() {
+        //region Given
+        sut.add("test 1");
+        sut.add(2);
+        //endregion
+
+        //region When
+        String result = sut.toString();
+        //endregion
+
+        //region Then
+        assertTrue(
+                result.contains("test 1") &&
+                result.contains("2")
+        );
+
+        String str;
+        Assert.assertThat(
+                result,
+                allOf(
+                        containsString("2"),
+                        containsString("test 1")
+                )
+        );
+
+        Assertions.assertThat(result)
+            .contains("2")
+            .contains("test 1");
         //endregion
     }
 }
